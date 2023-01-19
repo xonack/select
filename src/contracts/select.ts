ee4f9f50d0173c21a90f9cdb0d47e02dd8383300
honack <jenshonack@gmail.com>
2023-01-18 17:08:15 -0800

    branch

--- src/contracts/select.ts
+++ src/contracts/select.ts
@@ -1,13 +1,17 @@
 import {
     assert,
+    bsv,
     FixedArray,
+    hash256,
     method,
     prop,
     PubKey,
     PubKeyHash,
     Ripemd160,
+    Sig,
     SmartContract,
     toByteString,
+    UTXO,
 } from 'scrypt-ts'
 
 export class Select extends SmartContract {
@@ -33,8 +37,69 @@ export class Select extends SmartContract {
         this.winner = Ripemd160(toByteString('00'))
     }
 
+    // @method()
+    // public select() {
+    //     assert(this.open, 'nope')
+    // }
+
     @method()
-    public select() {
-        assert(true, 'nope')
+    public select(sig: Sig, winner: PubKeyHash) {
+        assert(this.open, 'can`t select winner for closed bounty')
+        assert(
+            this.checkSig(sig, this.maker),
+            'only bounty maker can select winner - check signature'
+        )
+        // let isHunter = false;
+        // for(let i = 0; i < 3; i++) {
+        //     if(this.hunters[i] === winner){
+        //         isHunter = true;
+        //     }
+        // }
+        // assert(isHunter, 'selected winner did not submit content for the bounty');
+        // this.winner = winner;
+        // TODO send amount to winner
+        // create output for a P2PKH to winner
+        // close bounty
+        this.open = false
+        assert(!this.open, 'bounty failed to close')
+        // assert(this.ctx.hashOutputs == hash256(this.buildStateOutput(this.ctx.utxo.value)), 'not paying fee from utxos');
+    }
+
+    getDeployTx(utxos: UTXO[], initBalance: number): bsv.Transaction {
+        this.balance = initBalance
+        const tx = new bsv.Transaction().from(utxos).addOutput(
+            new bsv.Transaction.Output({
+                script: this.lockingScript,
+                satoshis: initBalance,
+            })
+        )
+        this.lockTo = { tx, outputIndex: 0 }
+        return tx
+    }
+
+    getSelectTx(
+        utxos: UTXO[],
+        prevTx: bsv.Transaction,
+        nextInst: Select,
+        sig: Sig,
+        winner: PubKeyHash
+    ): bsv.Transaction {
+        const inputIndex = 1
+        return new bsv.Transaction()
+            .from(utxos)
+            .addInputFromPrevTx(prevTx)
+            .setOutput(0, (tx: bsv.Transaction) => {
+                nextInst.lockTo = { tx, outputIndex: 0 }
+                return new bsv.Transaction.Output({
+                    script: nextInst.lockingScript,
+                    satoshis: this.balance,
+                })
+            })
+            .setInputScript(inputIndex, (tx: bsv.Transaction) => {
+                this.unlockFrom = { tx, inputIndex }
+                return this.getUnlockingScript((self) => {
+                    self.select(sig, winner)
+                })
+            })
     }
 }
--- tests/local/select.test.ts
+++ tests/local/select.test.ts
@@ -5,20 +5,24 @@ import {
     PubKey,
     PubKeyHash,
     Ripemd160,
+    Sig,
+    signTx,
     toByteString,
     toHex,
 } from 'scrypt-ts'
 import { Select } from '../../src/contracts/Select'
-import { newTx, inputIndex } from './util/txHelper'
+import { dummyUTXO, newTx, inputSatoshis } from './util/txHelper'
 
 describe('Test SmartContract `Select`', () => {
     before(async () => {
-        await Select.compile() // asm
+        await Select.compile()
     })
 
     it('should pass with valid hunter', async () => {
         // constructor arguments
         const tx = newTx()
+        const inputIndex = 1
+        const utxos = [dummyUTXO]
         const privateKey = bsv.PrivateKey.fromRandom('testnet')
         const publicKey = bsv.PublicKey.fromPrivateKey(privateKey)
         const pubKey = PubKey(toHex(publicKey))
@@ -27,13 +31,54 @@ describe('Test SmartContract `Select`', () => {
         const hunter3: Ripemd160 = Ripemd160(toByteString('03'))
         const hunters: FixedArray<PubKeyHash, 3> = [hunter1, hunter2, hunter3]
         // create a genesis instance
-        const demo = new Select(pubKey, hunters)
+        const demo = new Select(pubKey, hunters).markAsGenesis()
+        // construct a transaction for deployment
+        const deployTx = demo.getDeployTx(utxos, 1)
         // unlockFrom
-        demo.unlockFrom = { tx, inputIndex }
-        const result = demo.verify(() => {
-            console.log(demo.open)
-            demo.select()
+        // demo.unlockFrom = { tx, inputIndex }
+        const makerSig = signTx(tx, privateKey, demo.lockingScript, inputSatoshis)
+        const newSelect = demo.next()
+        newSelect.winner = hunter2
+        newSelect.open = false
+        const selectTx = demo.getSelectTx(
+            utxos,
+            deployTx,
+            newSelect,
+            Sig(toHex(makerSig)),
+            hunter2
+        )
+        const result = demo.verify((self) => {
+            self.select(Sig(toHex(makerSig)), hunter2)
         })
-        expect(result.success).to.be.true
+        expect(result.success, result.error).to.be.true
     })
+
+    // // fails when accessing stateful variables - probably does not properly handle preimage
+    // it('should pass with valid hunter', async () => {
+    //     const inputIndex = 0;
+    //     const tx = newTx()
+    //     // constructor arguments
+    //     const privateKey = bsv.PrivateKey.fromRandom('testnet')
+    //     const publicKey = bsv.PublicKey.fromPrivateKey(privateKey)
+    //     const pubKey = PubKey(toHex(publicKey))
+    //     const hunter1: Ripemd160 = Ripemd160(toByteString('01'))
+    //     const hunter2: Ripemd160 = Ripemd160(toByteString('02'))
+    //     const hunter3: Ripemd160 = Ripemd160(toByteString('03'))
+    //     const hunters: FixedArray<PubKeyHash, 3> = [hunter1, hunter2, hunter3]
+    //     // create a genesis instance
+    //     const demo = new Select(pubKey, hunters)
+    //     // unlockFrom
+    //     demo.unlockFrom = { tx, inputIndex }
+    //     const result = demo.verify(() => {
+    //         const makerSig = signTx(
+    //             tx,
+    //             privateKey,
+    //             demo.lockingScript,
+    //             inputSatoshis
+    //         );
+    //         demo.select(Sig(toHex(makerSig)), hunter2);
+    //         // demo.select();
+    //     })
+    //     expect(result.success, result.error).to.be.true
+    // })
 })
